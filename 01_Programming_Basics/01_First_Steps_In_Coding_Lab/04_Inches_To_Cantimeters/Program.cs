10a0b81958412406a5ac748f39f550be4a449bc7
devailo <devailo.3d@gmail.com>
2021-10-28 13:43:19 +0300

    uploads

---
+++ 01_Programming_Basics/01_First_Steps_In_Coding_Lab/04_Inches_To_Cantimeters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace inch_cm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double conv = a * 2.54;
+
+            Console.WriteLine(conv);
+        }
+    }
+}
