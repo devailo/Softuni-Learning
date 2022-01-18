cb9958e81d027c0930e3b52f0b13d8acb2a8779c
devailo <devailo.3d@gmail.com>
2022-01-18 17:29:45 +0200

    uploads

--- 05_JavaScript_Advanced/04_JS_Arrays_Excercise/09_MagicMatrices.js
+++ 05_JavaScript_Advanced/04_JS_Arrays_Excercise/09_MagicMatrices.js
@@ -1,10 +1,20 @@
-function demo(array){
+function demo(array) {
 
-    let arrSort = array.sort((a,b) => a.length - b.length || a.localeCompare(b));
-   
-    console.log(arrSort.join('\n'));
+    let sum = array[0].reduce((a, b) => a + b);
+
+    for (let i = 0; i < array[0].length; i++) {
+      
+        let currentColSum = 0;
+
+        for (let j = 0; j < array.length; j++) {
+            currentColSum += array[j][i];
+        }
+        if (currentColSum !== sum) {
+            return false;
+        }
+    }
+    return true;
 }
 demo([[4, 5, 6],
-    [6, 5, 4],
-    [5, 5, 5]]
-   );
+[6, 5, 4],
+[5, 5, 5]]);
