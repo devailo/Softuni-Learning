22b6158c68885f3b26c73d0c47a8e72ea54abca5
devailo <devailo.3d@gmail.com>
2022-02-07 21:50:15 +0200

    uploads

--- 05_JavaScript_Advanced/10_Dom_Manipulations_And_Events_Exercise/02_Time_Converter/solution.js
+++ 05_JavaScript_Advanced/10_Dom_Manipulations_And_Events_Exercise/02_Time_Converter/solution.js
@@ -1,4 +1,40 @@
 function attachEventsListeners() {
+    let days = document.getElementById('days');
+    let hours = document.getElementById('hours');
+    let minutes = document.getElementById('minutes');
+    let seconds = document.getElementById('seconds');
 
-    console.log('TODO:...');
+    let units = {
+        days: 1,
+        hours: 24,
+        minutes: 1440,
+        seconds: 86400
+    }
+
+    document.getElementById('daysBtn').addEventListener('click', onConvert)
+    document.getElementById('hoursBtn').addEventListener('click', onConvert)
+    document.getElementById('minutesBtn').addEventListener('click', onConvert)
+    document.getElementById('secondsBtn').addEventListener('click', onConvert)
+
+    function convert(value, unit) {
+        let days = value / units[unit];
+
+        return {
+            days: days,
+            hours: days*units.hours,
+            minutes: days*units.minutes,
+            seconds: days*units.seconds
+        };
+    }
+
+    function onConvert(event) {
+        let input = event.target.parentElement.querySelector('input[type="text"]');
+
+        let time = convert(Number(input.value), input.id);
+
+        days.value = time.days;
+        hours.value = time.hours;
+        minutes.value = time.minutes;
+        seconds.value = time.seconds;
+    }
 }
--- 05_JavaScript_Advanced/10_Dom_Manipulations_And_Events_Exercise/04_Fill_Dropdown/dropdown.js
+++ 05_JavaScript_Advanced/10_Dom_Manipulations_And_Events_Exercise/04_Fill_Dropdown/dropdown.js
@@ -1,3 +1,19 @@
 function addItem() {
-    console.log('TODO:...');
+    let itemText = document.getElementById('newItemText').value;
+    let itemValue = document.getElementById('newItemValue').value;
+
+    let option = document.createElement('option');
+
+    option.text = itemText;
+    option.value = itemValue;
+
+    let menu = document.getElementById('menu');
+    if (itemText !== '' && itemValue !== '') {
+        menu.appendChild(option);
+    }
+ 
+
+    document.getElementById('newItemText').value = '';
+    document.getElementById('newItemValue').value = '';
+
 }
